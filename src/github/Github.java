3f58ee76f604b507573e114ca1bf5584cc452962
JohnLinus <john123linus@gmail.com>
2020-01-24 13:03:36 +0100

    id added

--- src/github/Github.java
+++ src/github/Github.java
@@ -5,6 +5,25 @@ public class Github {
     public class person{
         int age;
         String name;
+        int id;
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+        
+        
         
         
     }
