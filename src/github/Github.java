2bf0da002ec5547e3bde986ac361662c090b758d
JohnLinus <john123linus@gmail.com>
2020-01-24 12:51:45 +0100

    first commit

---
+++ src/github/Github.java
@@ -0,0 +1,17 @@
+package github;
+
+public class Github {
+    
+    public class person{
+        int age;
+        String name;
+        
+        
+    }
+
+    public static void main(String[] args) {
+        
+        
+
+    }
+}
